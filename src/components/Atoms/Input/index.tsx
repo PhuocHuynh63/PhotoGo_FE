75157a6d35262469afac65bcc195ee624309750a
Huỳnh Minh Phước <phuochmse171830@fpt.edu.vn>
2025-04-09 23:59:35 +0700

    Add error handling styles and update Input component for error display

--- src/components/Atoms/Input/index.tsx
+++ src/components/Atoms/Input/index.tsx
@@ -1,11 +1,11 @@
 import styles from './index.module.scss';
 
 export default function Input(props: ICOMPONENTS.InputProps) {
-    const { width, height, fontSize, style, ...rest } = props;
+    const { width, height, fontSize, style, className, ...rest } = props;
 
     return (
         <input
-            className={`${styles.input} `}
+            className={[styles.input, className].filter(Boolean).join(' ')}
             style={{
                 width,
                 height,
