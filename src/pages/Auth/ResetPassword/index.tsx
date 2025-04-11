d20067d8e5838ccbd3763ad9019a923d50e5f3f4
Huỳnh Minh Phước <phuochmse171830@fpt.edu.vn>
2025-04-11 14:21:19 +0700

    Enhance reset password functionality with confirm password validation and error handling

--- src/pages/Auth/ResetPassword/index.tsx
+++ src/pages/Auth/ResetPassword/index.tsx
@@ -12,6 +12,8 @@ import TransitionWrapper from "@components/Atoms/TransitionWrapper"
 import { ArrowLeft, KeyRoundIcon } from "lucide-react"
 import { useRouter } from "next/navigation"
 import { useGetLocalStorage } from "@utils/hooks/localStorage"
+import { useEffect } from "react"
+import toast from "react-hot-toast"
 
 const ResetPassswordPage = () => {
     //#region define variables
@@ -24,97 +26,131 @@ const ResetPassswordPage = () => {
     const {
         register,
         handleSubmit,
+        setValue,
+        watch,
         formState: { errors },
     } = useForm<IUserResetPasswordRequest>({
         resolver: zodResolver(UserResetPasswordRequest),
     })
 
     const onSubmit = (data: IUserResetPasswordRequest) => {
-        router.push(ROUTES.AUTH.VERIFY_OTP);
-        localStorage.setItem('email', data.email);
+        localStorage.removeItem('email');
         console.log(data);
+        toast.success('Đặt lại mật khẩu thành công')
+        router.push(ROUTES.PUBLIC.HOME)
     }
     //#endregion
 
+    /**
+       * Handle confirm password
+       * @param value 
+       * @returns 
+       */
+    const handleConfirmPassword = (value: string) => {
+        if (value !== watch('newPassword')) {
+            return 'Password does not match';
+        }
+        return true;
+    };
+    //---------------------------Handle confirm password---------------------------//
+
+
+    //#region check if email is exist in local storage
+    useEffect(() => {
+        if (!isReady) return;
+        if (!value) {
+            router.push(ROUTES.AUTH.FORGOT_PASSWORD)
+        } else {
+            setValue('email', value)
+        }
+    }, [isReady, value, router])
+    //#endregion
+
     return (
-        <TransitionWrapper>
-            {/* Main card container */}
-            <div className="w-full max-w-lg bg-white rounded-xl overflow-hidden shadow-xl flex flex-col md:flex-row">
-                {/* Forgot - form */}
-                <div className="w-full p-8 md:p-12">
-                    {/* Center logo */}
-                    <div className="flex justify-center items-center mb-4">
-                        <div className="rounded-full flex justify-center items-center w-16 h-16 bg-grey">
-                            <KeyRoundIcon className="w-8 h-8 text-primary font-bold" />
-                        </div>
-                    </div>
-
-                    <div className="flex flex-col items-center">
-                        <h1 className="text-2xl font-bold mb-2">Đặt lại mật khẩu</h1>
-                        <p className="text-description text-center mb-8 whitespace-pre-line">Nhập và ghi nhớ mật khẩu mới</p>
-                    </div>
-
-                    <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-                        {/* Email */}
-                        <Input type="hidden" defaultValue={value || ''} {...register('email')} />
-
-                        {/* New Password */}
-                        <div className="flex flex-col space-y-2  mb-2">
-                            <label htmlFor="newPassword" className="text-sm font-medium">
-                                Mật khẩu mới
-                            </label>
-
-                            <div className="flex flex-col space-y-2">
-                                <Input
-                                    id="newPassword"
-                                    type="newPassword"
-                                    togglePassword={true}
-                                    placeholder="Nhập mật khẩu mới"
-                                    {...register("newPassword")}
-                                    className={errors.newPassword ? 'input-error' : ''}
-                                />
-                                {errors.newPassword && <span className="text-red-500 text-sm">{errors.newPassword?.message}</span>}
+        <div className="w-full max-w-4/12">
+            <TransitionWrapper className="bg-white rounded-xl overflow-hidden shadow-xl  md:flex-row">
+                {/* Main card container */}
+                <div className="flex flex-col">
+                    {/* Forgot - form */}
+                    <div className="w-full p-8 md:p-12">
+                        {/* Center logo */}
+                        <div className="flex justify-center items-center mb-4">
+                            <div className="rounded-full flex justify-center items-center w-16 h-16 bg-grey">
+                                <KeyRoundIcon className="w-8 h-8 text-primary font-bold" />
                             </div>
                         </div>
 
-                        {/* Confirm Password */}
-                        <div className="flex flex-col space-y-2  mb-2">
-                            <label htmlFor="confirmPassword" className="text-sm font-medium">
-                                Mật khẩu mới
-                            </label>
-
-                            <div className="flex flex-col space-y-2">
-                                <Input
-                                    id="confirmPassword"
-                                    type="confirmPassword"
-                                    togglePassword={true}
-                                    placeholder="Nhập mật khẩu mới"
-                                    {...register("confirmPassword")}
-                                    className={errors.confirmPassword ? 'input-error' : ''}
-                                />
-                                {errors.confirmPassword && <span className="text-red-500 text-sm">{errors.confirmPassword?.message}</span>}
-                            </div>
+                        <div className="flex flex-col items-center">
+                            <h1 className="text-2xl font-bold mb-2">Đặt lại mật khẩu</h1>
+                            <p className="text-description text-center mb-8 whitespace-pre-line">Nhập và ghi nhớ mật khẩu mới</p>
                         </div>
 
-                        <Button type="submit" style={{ width: "100%" }}>
-                            Tiếp tục
-                        </Button>
-                    </form>
+                        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+                            {/* Email */}
+                            <Input type="hidden" defaultValue={value || ''} {...register('email')} />
 
-                    <p className="flex mt-5 justify-center items-center text-description-title">
-                        <ArrowLeft size={20} className="text-dark mr-2" />
-                        <Link href={ROUTES.AUTH.LOGIN} className="font-sm text-dark hover:underline">
-                            Quay lại trang đăng nhập
-                        </Link>
-                    </p>
+                            {/* New Password */}
+                            <div className="flex flex-col space-y-2  mb-2">
+                                <label htmlFor="newPassword" className="text-sm font-medium">
+                                    Mật khẩu mới
+                                </label>
+
+                                <div className="flex flex-col space-y-2">
+                                    <Input
+                                        id="newPassword"
+                                        type="password"
+                                        togglePassword={true}
+                                        placeholder="Nhập mật khẩu mới"
+                                        {...register("newPassword")}
+                                        className={errors.newPassword ? 'input-error' : ''}
+                                    />
+                                    {errors.newPassword && <span className="text-red-500 text-sm">{errors.newPassword?.message}</span>}
+                                </div>
+                            </div>
+
+                            {/* Confirm Password */}
+                            <div className="flex flex-col space-y-2 mb-2">
+                                <label htmlFor="confirmPassword" className="text-sm font-medium">
+                                    Xác nhận mật khẩu
+                                </label>
+
+                                <div className="flex flex-col space-y-2 mb-2">
+                                    <Input
+                                        id="confirmPassword"
+                                        type="password"
+                                        togglePassword={true}
+                                        placeholder="Nhập mật khẩu mới"
+                                        {...register("confirmPassword", {
+                                            required: true,
+                                            validate: handleConfirmPassword,
+                                        })}
+                                        className={errors.confirmPassword ? 'input-error' : ''}
+                                    />
+                                    {errors.confirmPassword && <span className="text-red-500 text-sm">{errors.confirmPassword?.message}</span>}
+                                </div>
+                            </div>
+
+                            <Button type="submit" style={{ width: "100%" }}>
+                                Xác nhận
+                            </Button>
+                        </form>
+
+                        <p className="flex mt-5 justify-center items-center text-description-title">
+                            <ArrowLeft size={20} className="text-dark mr-2" />
+                            <Link href={ROUTES.AUTH.FORGOT_PASSWORD} className="font-sm text-dark hover:underline" onClick={() => localStorage.removeItem('email')}>
+                                Quay lại
+                            </Link>
+                        </p>
+                    </div>
+                    {/* --- End --- */}
                 </div>
-                {/* --- End --- */}
-            </div>
+            </TransitionWrapper>
             <div className="flex justify-center items-center mt-8">
                 <span className="text-description">Bạn cần trợ giúp? <Link href={ROUTES.PUBLIC.CONTACT} className="text-primary font-bold">Liên hệ với chúng tôi</Link>
                 </span>
             </div>
-        </TransitionWrapper>
+        </div>
+
     )
 }
 
