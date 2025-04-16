76d42b6148c059ba8367462a11575cfb11ede45a
Huỳnh Minh Phước <118465492+PhuocHuynh63@users.noreply.github.com>
2025-04-17 01:32:32 +0700

    Refactor checkout components: add Confirm component and update CheckoutPage to include it in the rendering logic

--- src/pages/Member/Checkout/index.tsx
+++ src/pages/Member/Checkout/index.tsx
@@ -4,6 +4,7 @@ import { useCheckoutStep } from '@stores/checkout/selector'
 import React from 'react'
 import Method from './Left/Method'
 import Infomation from './Left/Infomation'
+import Confirm from './Left/Confirm'
 
 const CheckoutPage = () => {
     const currentStep = useCheckoutStep()
@@ -15,7 +16,7 @@ const CheckoutPage = () => {
                 : currentStep === 2 ?
                     <Infomation />
                     : <div>
-                        cc Tina
+                        <Confirm />
                     </div>
             }
         </>
