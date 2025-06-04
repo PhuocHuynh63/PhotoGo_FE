2471e9c64b8107647e3e65c14fbff090a6d2a803
Adu Ang Du <vunguyen003pv@gmail.com>
2025-06-04 22:37:52 +0700

    refactor: Enhance search package functionality and update service models
    - Implemented a new async function to fetch packages based on search parameters in SearchPackagePage.
    - Updated SearchPackage component to accept packages and pagination data as props.
    - Refactored Right component to utilize the new packages and pagination structure, improving data handling and display.
    - Modified service package model to replace price and duration with minPrice and maxPrice.
    - Added a new method in packageServices for fetching filtered packages with pagination support.
    - Cleaned up unused code and improved type definitions for better clarity and maintainability.

--- src/app/(user)/(layout)/(guest)/search/packages/page.tsx
+++ src/app/(user)/(layout)/(guest)/search/packages/page.tsx
@@ -1,10 +1,35 @@
-import SearchPackagePage from "@pages/Public/Search/SearchPackage";
+import { IServicePackagesListResponse } from "@models/servicePackages/response.model";
+import SearchPackage from "@pages/Public/Search/SearchPackage";
+import packageServices from "@services/packageServices";
 
-export default function SearchVendor() {
+async function getPackages({ searchParams }: SERVERS.SearchPackagePageProps) {
+    const resolvedParams = await searchParams;
 
+    const queryParams = new URLSearchParams();
+
+    if (resolvedParams.name) queryParams.append('name', resolvedParams.name as string);
+    if (resolvedParams.address) queryParams.append('location', resolvedParams.address as string);
+    if (resolvedParams.minPrice) queryParams.append('minPrice', resolvedParams.minPrice as string);
+    if (resolvedParams.maxPrice) queryParams.append('maxPrice', resolvedParams.maxPrice as string);
+    if (resolvedParams.minRating) queryParams.append('minRating', resolvedParams.minRating as string);
+    // if (resolvedParams.date) queryParams.append('date', resolvedParams.date as string);
+    if (resolvedParams.current) queryParams.append('current', resolvedParams.current as string);
+    if (resolvedParams.sortBy) queryParams.append('sortBy', resolvedParams.sortBy as string);
+    if (resolvedParams.sortDirection) queryParams.append('sortDirection', resolvedParams.sortDirection as string);
+
+    const response = await packageServices.getPackagesWithFilter(queryParams) as IServicePackagesListResponse;
+    if (!response.data) {
+        throw new Error('No data received')
+    }
+    return response.data;
+}
+
+export default async function SearchPackagePage({ searchParams }: SERVERS.SearchPackagePageProps) {
+    const packagesData = await getPackages({ searchParams });
+    console.log(packagesData)
     return (
         <>
-            <SearchPackagePage />
+            <SearchPackage packages={packagesData.data} pagination={packagesData.pagination} />
         </>
     );
 }
