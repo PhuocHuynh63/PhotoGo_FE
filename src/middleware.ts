eb9d3391f2cdfac871268bfe961cafa91ad85966
PhuocHuynh63 <phuochmse171830@fpt.edu.vn>
2025-06-23 17:52:34 +0700

    feat: Refactor authentication and payment error handling middleware

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { authConfig, authMatcher } from "@middlewares/auth";
+import { handleStatus, paymentErrorMatcher } from "@middlewares/status";
+import { NextRequest, NextResponse } from "next/server";
+import { withAuth } from "next-auth/middleware";
+
+function mainMiddleware(request: NextRequest) {
+    const { pathname } = request.nextUrl;
+
+    /**
+     * Check payment status
+     * - Payment error page
+     */
+    if (paymentErrorMatcher.includes(pathname)) {
+        const response = handleStatus(request);
+        if (response) return response;
+    }
+
+    return NextResponse.next();
+}
+
+const fullMiddleware = withAuth(mainMiddleware, authConfig);
+
+export default fullMiddleware;
+
+export const config = {
+    matcher: [
+        ...paymentErrorMatcher,
+        ...authMatcher,
+    ],
+};
