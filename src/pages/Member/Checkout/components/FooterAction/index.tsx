65357676edf631d4b782382324ae08564777c48f
Huỳnh Minh Phước <118465492+PhuocHuynh63@users.noreply.github.com>
2025-06-27 23:55:38 +0700

    feat: Add form validation and loading state to booking process

--- src/pages/Member/Checkout/components/FooterAction/index.tsx
+++ src/pages/Member/Checkout/components/FooterAction/index.tsx
@@ -5,7 +5,7 @@ import BookingService from '@services/booking'
 import { useCheckoutStep, useFormBooking, useIsValidStep, useSetCheckoutStep } from '@stores/checkout/selectors'
 import { ArrowRight } from 'lucide-react'
 import { useRouter } from 'next/navigation'
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import toast from 'react-hot-toast'
 
@@ -35,6 +35,7 @@ const FooterAction = () => {
     /**
      * Handle form booking data
      */
+    const [loading, setLoading] = useState<boolean>(false)
     const formBooking = useFormBooking()
     const { handleSubmit } = useForm({
         defaultValues: formBooking,
@@ -44,6 +45,7 @@ const FooterAction = () => {
     const onSubmit = async () => {
         const { userId, serviceConceptId, ...spread } = formBooking
         try {
+            setLoading(true)
             const res = await BookingService.createBooking(
                 userId,
                 serviceConceptId,
@@ -58,39 +60,43 @@ const FooterAction = () => {
         } catch (error) {
             console.error('Error creating booking:', error)
             return
+        } finally {
+            setLoading(false)
         }
+        //------------------------------End-----------------------------//
+
+        const handleBack = () => {
+            if (currentStep === 1) router.back()
+            else setCurrentStep(currentStep - 1)
+        }
+
+
+        return (
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="flex justify-between items-center mt-8">
+                    <button type="button" onClick={handleBack} className='cursor-pointer ml-2'>Quay lại</button>
+                    <Button
+                        className="bg-primary hover:bg-[#e8935d]"
+                        disabled={!isDisabled}
+                        type={currentStep === 3 ? 'submit' : 'button'}
+                        isLoading={currentStep === 3 && loading}
+                        loadingText="Đang xử lý thanh toán..."
+                        onClick={
+                            currentStep < 3
+                                ? (e) => {
+                                    e.preventDefault();
+                                    setCurrentStep(currentStep + 1);
+                                }
+                                : undefined
+                        }
+                    >
+                        {currentStep < 3 ? 'Tiếp tục' : 'Thanh toán'}
+                        <ArrowRight className="ml-2 h-4 w-4" />
+                    </Button>
+                </div>
+            </form>
+        )
     }
-    //------------------------------End-----------------------------//
-
-    const handleBack = () => {
-        if (currentStep === 1) router.back()
-        else setCurrentStep(currentStep - 1)
-    }
-
-
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="flex justify-between items-center mt-8">
-                <button type="button" onClick={handleBack} className='cursor-pointer ml-2'>Quay lại</button>
-                <Button
-                    className="bg-primary hover:bg-[#e8935d]"
-                    disabled={!isDisabled}
-                    type={currentStep === 3 ? 'submit' : 'button'}
-                    onClick={
-                        currentStep < 3
-                            ? (e) => {
-                                e.preventDefault();
-                                setCurrentStep(currentStep + 1);
-                            }
-                            : undefined
-                    }
-                >
-                    {currentStep < 3 ? 'Tiếp tục' : 'Thanh toán'}
-                    <ArrowRight className="ml-2 h-4 w-4" />
-                </Button>
-            </div>
-        </form>
-    )
 }
 
 export default FooterAction
