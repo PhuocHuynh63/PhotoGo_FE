3a0737465330cdd3db39ff8ee7c01389add17491
Adu Ang Du <vunguyen003pv@gmail.com>
2025-07-01 01:57:06 +0700

    feat: enhance vendor branch management and UI components
    - Updated the VendorBranches component to fetch branches based on the authenticated user's ID.
    - Refined the BranchList component to manage branch data more effectively, including improved filtering and display of branch information.
    - Enhanced the BranchModal to support additional address fields and validation for branch creation and editing.
    - Integrated loading states and error handling for better user experience during branch operations.

--- src/app/(user)/(no-layout)/vendor/branches/page.tsx
+++ src/app/(user)/(no-layout)/vendor/branches/page.tsx
@@ -1,8 +1,14 @@
+import { authOptions } from "@lib/authOptions"
 import BranchList from "@pages/Vendor/Branches"
+import vendorService from "@services/vendors"
+import { METADATA } from "../../../../../types/IMetadata"
+import { getServerSession } from "next-auth"
+import { ILocation } from "@models/location/common.model"
+import { IVendorResponse } from "@models/vendor/response.model"
 
-async function getBranches() {
-    // TODO: Replace with actual API call
-    return []
+async function getBranches(vendorId: string) {
+    const response = await vendorService.getVendorByUserId(vendorId)
+    return response
 }
 
 async function getServices() {
@@ -11,9 +17,13 @@ async function getServices() {
 }
 
 export default async function VendorBranches() {
-    const branches = await getBranches()
+    const session = await getServerSession(authOptions) as METADATA.ISession;
+
+    const branches = await getBranches(session.user.id) as IVendorResponse
+    const branchesData = branches.data?.locations as ILocation[] ?? []
+    const vendorId = branches.data?.id
     const services = await getServices()
 
-    return <BranchList branches={branches} services={services} />
+    return <BranchList branches={branchesData} services={services} vendorId={vendorId} />
 }
 
--- src/models/location/response.model.ts
+++ src/models/location/response.model.ts
@@ -1,6 +1,10 @@
 import { z } from "zod";
 import { PaginationModel } from "@models/metadata";
+import { LocationModel } from "./common.model";
 
+/**
+ * Model of All Location
+ */
 export const AllLocationResponseModel = z.object({
     statusCode: z.number(),
     message: z.string(),
@@ -11,3 +15,17 @@ export const AllLocationResponseModel = z.object({
 });
 
 export type IAllLocationResponse = z.infer<typeof AllLocationResponseModel>;
+//----------------------End----------------------//
+
+
+/**
+ * Model of Location
+ */
+export const LocationResponseModel = z.object({
+    statusCode: z.number(),
+    message: z.string(),
+    data: LocationModel
+});
+
+export type ILocationResponse = z.infer<typeof LocationResponseModel>;
+//----------------------End----------------------//
