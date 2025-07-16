3c8ffe5733528e5c357e6c17a0a4ffb57ca16388
PhuocHuynh63 <phuochmse171830@fpt.edu.vn>
2025-07-17 01:43:56 +0700

    ,

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,7 +25,7 @@ export async function middleware(req: NextRequest) {
         return NextResponse.next();
     }
     const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-    
+
 
     if (!token) {
         const loginUrl = new URL(ROUTES.AUTH.LOGIN, req.url);
