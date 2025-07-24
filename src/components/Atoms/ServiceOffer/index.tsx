c1d47a00921863ead4ddf978922b3dda818d4f60
PhuocHuynh63 <phuochmse171830@fpt.edu.vn>
2025-07-24 14:30:06 +0700

    Refactor ServiceOffer and ServiceCard components for improved role-based rendering and description handling
    - Updated ButtonServiceOffer to conditionally render the button based on user roles, enhancing user experience.
    - Modified ServiceCard to use dangerouslySetInnerHTML for rendering descriptions, allowing for better HTML formatting and display control.

--- src/components/Atoms/ServiceOffer/index.tsx
+++ src/components/Atoms/ServiceOffer/index.tsx
@@ -19,24 +19,28 @@ const ButtonServiceOffer = ({ roleName }: { roleName: string | undefined }) => {
     //-----------------------------End-----------------------------//
 
     return (
-        <button className="cursor-pointer relative px-6 py-2 text-white font-semibold rounded-lg overflow-hidden transition-all duration-300 hover:scale-105 animate-gradient" onClick={handleRouter}>
-            <div
-                className="absolute inset-0 animate-pulse"
-                style={{
-                    background:
-                        "linear-gradient(270deg, #8B5CF6, #A855F7, #EC4899, #F97316, #EAB308, #84CC16, #10B981, #06B6D4)",
-                    backgroundSize: "400% 400%",
-                    animation: "gradient 3s ease infinite",
-                }}
-            />
-            {roleName === ROLE.CUSTOMER && (
-                <span className="relative z-10">🔥 Ưu đãi hội viên</span>
-            )}
+        <>
+            {roleName !== ROLE.ADMIN && (
+                <button className="cursor-pointer relative px-6 py-2 text-white font-semibold rounded-lg overflow-hidden transition-all duration-300 hover:scale-105 animate-gradient" onClick={handleRouter}>
+                    <div
+                        className="absolute inset-0 animate-pulse"
+                        style={{
+                            background:
+                                "linear-gradient(270deg, #8B5CF6, #A855F7, #EC4899, #F97316, #EAB308, #84CC16, #10B981, #06B6D4)",
+                            backgroundSize: "400% 400%",
+                            animation: "gradient 3s ease infinite",
+                        }}
+                    />
+                    {roleName === ROLE.CUSTOMER && (
+                        <span className="relative z-10">🔥 Ưu đãi hội viên</span>
+                    )}
 
-            {roleName === ROLE.VENDOR_OWNER && (
-                <span className="relative z-10">🔥 Ưu đãi nhà cung cấp</span>
+                    {roleName === ROLE.VENDOR_OWNER && (
+                        <span className="relative z-10">🔥 Ưu đãi nhà cung cấp</span>
+                    )}
+                </button>
             )}
-        </button>
+        </>
     )
 }
 
