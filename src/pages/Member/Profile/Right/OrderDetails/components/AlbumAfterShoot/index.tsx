8ab4f8a615a678b587ccef53c4244995af29dee3
PhuocHuynh63 <phuochmse171830@fpt.edu.vn>
2025-07-27 22:45:05 +0700

    Update AblumAfterShoot component to conditionally render Google Drive link based on vendorAlbums data

--- src/pages/Member/Profile/Right/OrderDetails/components/AlbumAfterShoot/index.tsx
+++ src/pages/Member/Profile/Right/OrderDetails/components/AlbumAfterShoot/index.tsx
@@ -57,7 +57,7 @@ const AblumAfterShoot = ({ id, title, subTitle, skeletonCount, vendorAlbums, isL
       <Card className="p-8">
         <h3 className="text-2xl font-bold mb-2 text-gray-800">{title}</h3>
         <p className="text-gray-500 mb-6">{subTitle}</p>
-        {id === albumComponent.ALBUM_AFTER_SHOOT_GOOGLE_DRIVE && (
+        {id === albumComponent.ALBUM_AFTER_SHOOT_GOOGLE_DRIVE && vendorAlbums?.driveLink && (
           <div className="text-gray-500 mb-6">
             <a href={getGoogleDriveLink()} target="_blank" rel="noopener noreferrer" className='text-blue-500 underline'>
               {getGoogleDriveLink()}
